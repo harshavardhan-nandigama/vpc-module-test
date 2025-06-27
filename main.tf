3d92ae7df87943cc3d77f4efcc3d95046cb4e746
unknown <harha@gmail.com>
2025-06-27 10:34:03 +0530

    terrafrom

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    source = "git::https://github.com/harshavardhan-nandigama/terraform-aws-vpc.git?ref=main"
+    # source = "../terraform-aws-vpc"
    /*  project = "roboshop"
     environment = "dev"
     public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
@@ -10,4 +11,5 @@ public_subnet_cidrs = var.public_subnet_cidrs
 private_subnet_cidrs = var.private_subnet_cidrs
 database_subnet_cidrs = var.database_subnet_cidrs
 
+is_peering_required = true 
 }
